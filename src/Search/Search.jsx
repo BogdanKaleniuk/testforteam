7fbb34c3fb4b896e95c020ae87bc9e3674b6624d
Bogdan Kaleniuk <bogdankaleniuk95@gmail.com>
2024-10-24 15:27:53 +0300

    next2

--- src/Search/Search.jsx
+++ src/Search/Search.jsx
@@ -1,28 +1,10 @@
-import { useState } from "react";
-import SearchBar from "./SearchBar";
+import PRODUCTS from "./products";
+import FilterableProductTable from "./Filterable";
 
-function FilterableProductTable({ products }) {
-  const [filterText, setFilterText] = useState("");
-  const [inStockOnly, setInStockOnly] = useState(false);
-
-  return (
-    <div>
-      <SearchBar
-        filterText={filterText}
-        inStockOnly={inStockOnly}
-        onFilterTextChange={setFilterText}
-        onInStockOnlyChange={setInStockOnly}
-      />
-      <ProductTable
-        products={products}
-        filterText={filterText}
-        inStockOnly={inStockOnly}
-      />
-    </div>
-  );
+export default function Search() {
+  return <FilterableProductTable products={PRODUCTS} />;
 }
-
-function ProductCategoryRow({ category }) {
+export function ProductCategoryRow({ category }) {
   return (
     <tr>
       <th colSpan="2">{category}</th>
@@ -30,7 +12,7 @@ function ProductCategoryRow({ category }) {
   );
 }
 
-function ProductRow({ product }) {
+export function ProductRow({ product }) {
   const name = product.stocked ? (
     product.name
   ) : (
@@ -43,53 +25,4 @@ function ProductRow({ product }) {
       <td>{product.price}</td>
     </tr>
   );
-}
-
-function ProductTable({ products, filterText, inStockOnly }) {
-  const rows = [];
-  let lastCategory = null;
-
-  products.forEach((product) => {
-    if (product.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
-      return;
-    }
-    if (inStockOnly && !product.stocked) {
-      return;
-    }
-    if (product.category !== lastCategory) {
-      rows.push(
-        <ProductCategoryRow
-          category={product.category}
-          key={product.category}
-        />
-      );
-    }
-    rows.push(<ProductRow product={product} key={product.name} />);
-    lastCategory = product.category;
-  });
-
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Price</th>
-        </tr>
-      </thead>
-      <tbody>{rows}</tbody>
-    </table>
-  );
-}
-
-const PRODUCTS = [
-  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
-  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
-  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
-  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
-];
-
-export default function Search() {
-  return <FilterableProductTable products={PRODUCTS} />;
 }
